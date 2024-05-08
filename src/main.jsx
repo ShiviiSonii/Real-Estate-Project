391bb5ff388f525772a9e914a7889ca50b4c6dc9
ShiviiSonii <1111dhruvsoni@gmail.com>
2024-05-08 12:36:27 +0530

    routes added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {RouterProvider, createBrowserRouter} from "react-router-dom"
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Profile from "./components/Profile.jsx"
+import Signin from "./components/SignIn.jsx"
+import Signout from "./components/SignUp.jsx"
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element : <Home/>
+  },
+  {
+    path: "/about",
+    element : <About/>
+  },
+  {
+    path: "/profile",
+    element : <Profile/>
+  },
+  {
+    path: "/sign-in",
+    element : <Signin/>
+  },
+  {
+    path: "/sign-out",
+    element : <Signout/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
