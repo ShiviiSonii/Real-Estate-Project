b93af94d5728304e15e6e04ca37979a89330353b
ShiviiSonii <1111dhruvsoni@gmail.com>
2024-05-09 12:13:21 +0530

    connected database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(()=> {
+        console.log("DB connected")
+    })
+    .catch((err) => {
+        console.log("Error occured while connecting the database")
+    })
+
 
 const app = express()
-const port = 3000
+const port = process.env.PORT | 3000
 
 app.listen(port, ()=> {
     console.log(`Listening on port ${port}`)
